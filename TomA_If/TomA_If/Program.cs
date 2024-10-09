5d9ec66037bfff3e748c48ac7ef941a8cae86d23
Tom Adriaens <tom.adriaens@romerocollege.be>
2024-10-09 11:46:08 +0200

    [LES] Keuze menu + keuze vd gebruiker opvangen

--- TomA_If/TomA_If/Program.cs
+++ TomA_If/TomA_If/Program.cs
@@ -10,6 +10,28 @@ namespace TomA_If
     {
         static void Main(string[] args)
         {
+            // Tom Adriaens
+            // 09/10/2024
+            // eten
+
+            // Velden 
+            byte _keuze = 0;
+            //Programma
+
+            // maak een keuzemenu
+            Console.WriteLine("Maak uw eetkeuze uit onderstaand menu:");
+            Console.WriteLine("\n   1) Kapsalon\n   2) Bicky Friet\n   3) Pizza");
+            
+            // Vraag keuze + opslaan
+            Console.Write("\n\nHet nummer van keuze: ");
+            _keuze = byte.Parse(Console.ReadLine());
+
+            // maake scherm leeg 
+            Console.Clear();
+
+
+
+
         }
     }
 }

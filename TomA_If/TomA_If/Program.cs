47b208519c52c6bd0a8ba2514dfacba76df92d4f
Tom Adriaens <tom.adriaens@romerocollege.be>
2024-10-10 13:20:40 +0200

    [LES] IF efficient

--- TomA_If/TomA_If/Program.cs
+++ TomA_If/TomA_If/Program.cs
@@ -18,48 +18,57 @@ namespace TomA_If
             byte _keuze = 0;
             //Programma
 
+
             // maak een keuzemenu
             Console.WriteLine("Maak uw eetkeuze uit onderstaand menu:");
             Console.WriteLine("\n   1) Kapsalon\n   2) Bicky Friet\n   3) Pizza");
-            
-            // Vraag keuze + opslaan
-            Console.Write("\n\nHet nummer van keuze: ");
-            _keuze = byte.Parse(Console.ReadLine());
-
-            // maake scherm leeg 
-            Console.Clear();
-
-            if (_keuze == 1)
+            try
             {
-                // geef de juiste tekst
-                Console.WriteLine("De prijs is 10 EUR");
 
+
+                // Vraag keuze + opslaan
+                Console.Write("\n\nHet nummer van keuze: ");
+                _keuze = byte.Parse(Console.ReadLine());
+
+                // maake scherm leeg 
+                Console.Clear();
+
+                if (_keuze == 1)
+                {
+                    // geef de juiste tekst
+                    Console.WriteLine("De prijs is 10 EUR");
+
+
+                }
+                else if (_keuze == 2)
+                {
+                    // geef de juiste tekst
+                    Console.WriteLine("De prijs is 8 EUR");
+
+                }
+                else if (_keuze == 3)
+                {
+                    // geef de juiste tekst
+                    Console.WriteLine("De prijs is 16 EUR");
+
+                }
+                else
+                {
+                    // geef de juiste tekst
+                    Console.WriteLine("U gaf geen juiste keuze in");
+                
+                }
                 Console.WriteLine("Druk op enter om af te sluiten");
                 Console.ReadKey();
 
             }
-            else if(_keuze == 2)
+            catch 
             {
-                // geef de juiste tekst
-                Console.WriteLine("De prijs is 8 EUR");
+                // maake scherm leeg 
+                Console.Clear();
 
-                Console.WriteLine("Druk op enter om af te sluiten");
-                Console.ReadKey();
-            }
-            else if (_keuze == 3)
-            {
-                // geef de juiste tekst
-                Console.WriteLine("De prijs is 16 EUR");
-
-                Console.WriteLine("Druk op enter om af te sluiten");
-                Console.ReadKey();
-            }
-            else
-            {
-                // geef de juiste tekst
-                Console.WriteLine("U gaf geen juiste keuze in");
-
-                Console.WriteLine("Druk op enter om af te sluiten");
+                //foutmelding
+                Console.WriteLine("U gaf geen getal in.\n\nDruk op enter om af te sluiten");
                 Console.ReadKey();
             }
 

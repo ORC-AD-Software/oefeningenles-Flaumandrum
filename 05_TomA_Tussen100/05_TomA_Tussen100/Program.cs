330416d7f350262e946cd31b774a606ecf2f7fbf
Tom Adriaens <tom.adriaens@romerocollege.be>
2024-10-18 10:02:08 +0200

    [LES] afgewert

--- 05_TomA_Tussen100/05_TomA_Tussen100/Program.cs
+++ 05_TomA_Tussen100/05_TomA_Tussen100/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +19,8 @@ namespace _05_TomA_Tussen100
             // Project tussen 100
 
             // velden 
-            byte _keuze = 0, getal = 0;
+            byte _keuze = 0, _getal = 0;
+            Boolean _herhalen1 = true;
 
             // Programma
             //Stap 1: Intro
@@ -33,8 +36,8 @@ namespace _05_TomA_Tussen100
                 //Stap 2: Toon menu(getal controleren, afsluiten)
                 Console.WriteLine("Maak uw keuze uit onderstaand menu: ");
                 Console.WriteLine("\n   1) Controleer een getal\n   2) Afsluiten");
-                Console.WriteLine("\n\nDruk op een toets om verder te gaan.");
-                Console.ReadKey();
+                
+
 
                 try
                 {
@@ -47,30 +50,91 @@ namespace _05_TomA_Tussen100
                     Console.Clear();
 
                     //Als controleren
-                    if (_keuze == 0)
+                    if (_keuze == 1)
                     {
-                        //Stap 4: Vraag getal +opslaan
+                        while (_herhalen1)
+                        {
+                            _herhalen1 = false;
+                            // SCherm leegmaken 
+                            Console.Clear();
 
-                        //Stap 5: controleer of tussen 0 en 100
+                            try
+                            {
 
-                        //Indien juist
-                        //Stap 6: 
-                        //		Als tussen 0 en 30: toon tekst
+                                //Stap 4: Vraag getal +opslaan
+                                Console.Write("Geef een getal in tussen 0 en 100: ");
+                                _getal = Byte.Parse(Console.ReadLine());
 
-                        //        Als tussen 31 en 50: toon tekst
+                                
 
-                        //        Als tussen 51 en 70: toon tekst
+                                //Stap 5: controleer of tussen 0 en 100
+                                //Indien fout
+                                if (_getal >100)
+                                {
+                                    // SCherm leegmaken 
+                                    Console.Clear();
 
-                        //        Als tussen 71 en 100: toon tekst
+                                    // foutmelding
+                                    Console.WriteLine("Het getal moet tussen 0 en 100 zijn!");
+                                    Console.WriteLine("\n\nDruk op een toets om opnieuw te proberen.");
+                                    Console.ReadKey();
+
+                                    _herhalen1 = true;
+
+                                }
+                                //Indien juist
+                                else
+                                {
+                                    //Stap 6: 
+                                    //		Als tussen 0 en 30: toon tekst
+                                    if (_getal<31)
+                                    {
+                                        Console.WriteLine($"U gaf {_getal.ToString()}in. \nDit getal ligt tussen 0 en 30.");
+                                    }
+                                    //        Als tussen 31 en 50: toon tekst
+                                    else if (_getal < 51)
+                                    {
+                                        Console.WriteLine($"U gaf {_getal.ToString()}in. \nDit getal ligt tussen 31 en 50.");
+                                    }
+                                    //        Als tussen 51 en 70: toon tekst
+                                    else if ( _getal < 71)
+                                    {
+                                        Console.WriteLine($"U gaf {_getal.ToString()}in. \nDit getal ligt tussen 51 en 70.");
+                                    }
+                                    //        Als tussen 71 en 100: toon tekst
+                                    else
+                                    {
+                                        Console.WriteLine($"U gaf {_getal.ToString()}in. \nDit getal ligt tussen 71 en 100.");
+
+                                    }
+                                    Console.WriteLine("\n\nDruk op een toets om verder te gaan.");
+                                    Console.ReadKey();
+
+                                }
+
+                            }
+                            catch
+                            {
+                                // SCherm leegmaken 
+                                Console.Clear();
+
+                                // foutmelding
+                                Console.WriteLine("Geen juiste invoer!");
+                                Console.WriteLine("\n\nDruk op een toets om opnieuw te proberen.");
+                                Console.ReadKey();
+
+                                _herhalen1 = true;
+                            }
+                        }
 
                     }
-                    
+
                     //Als afsluiten
                     else if (_keuze == 2)
                     {
                         //    Stap 7: sluit af
                         Console.WriteLine("Tot ziens!");
-                        Console.WriteLine("\n\nDruk op een toets om opnieuw te proberen.");
+                        Console.WriteLine("\n\nDruk op een toets om af te sluiten.");
                         Console.ReadKey();
                     }
 

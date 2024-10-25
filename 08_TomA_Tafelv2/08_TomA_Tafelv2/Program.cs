00c7b754998a85e1404ee9e0f6570ccfd3fcee0e
Tom Adriaens <tom.adriaens@romerocollege.be>
2024-10-25 08:39:19 +0200

    [LES] Start tafel van 2

---
+++ 08_TomA_Tafelv2/08_TomA_Tafelv2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_TomA_Tafelv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

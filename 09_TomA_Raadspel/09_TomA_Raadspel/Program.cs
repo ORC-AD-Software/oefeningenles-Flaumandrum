db448d936773f555d01fa7c3cf98918d62e39553
tomadriaens <tomadriaens@hotmail.com>
2024-11-04 10:45:45 +0100

    [LES] Poging tot invoegen ander project + tussenstap voor het 5 keer raden

--- 09_TomA_Raadspel/09_TomA_Raadspel/Program.cs
+++ 09_TomA_Raadspel/09_TomA_Raadspel/Program.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace _09_TomA_Raadspel
 {
@@ -21,7 +24,7 @@ namespace _09_TomA_Raadspel
 
             // Programma
             //Stap 1: Intro
-            Console.WriteLine("Welkom,\n\nIn dit spel moet je een getal raden tussen 0 en 100 (inclusier)");
+            Console.WriteLine("Welkom,\n\nIn dit spel moet je een getal raden tussen 0 en 100 (inclusief)");
             Console.WriteLine("Je hebt 5 mogelijkheden");
             Console.WriteLine("\nDruk op een toets om verder te gaan.");
             Console.ReadKey();
@@ -39,14 +42,59 @@ namespace _09_TomA_Raadspel
                 {
 
                     //Stap 3: Vraag de keuze + opslaan
-                    //Als 1:
-                    //Stap 4: maak een willekeurig raadgetal aan
-                    //Stap 5: Laat de gebruiker raden +opslaan, max 5 keer, blijf herhalen zolang niet geraden
-                    //    Als geraden: toon “Proficiat geraden” +verlaat lus
-                    //    Als raadgetal hoger: Toon het te raden getal is hoger”  
-                    //	  Als raadgetal lager: Toon het te raden getal is lager”
-                    //Als 2: Afsluiten
+                    Console.Write("\nGeef uw keuze: ");
+                    _keuze = Byte.Parse( Console.ReadLine());
 
+                    // Scherm leegmaken 
+                    Console.Clear();
+
+                    //Als 1:
+                    if (_keuze == 1)
+                    {
+                        //Stap 4: maak een willekeurig raadgetal aan
+                        _raadgetal = Convert.ToByte(_rdm.Next(101));
+
+                        //Stap 5: Laat de gebruiker raden +opslaan, max 5 keer, blijf herhalen zolang niet geraden
+
+                        Console.Write("\nGeef uw gok in: ");
+                        _gok = Byte.Parse(Console.ReadLine());
+                        //    Als geraden: toon “Proficiat geraden” +verlaat lus
+                        if(_raadgetal == _gok)
+                        {
+
+                        }
+                        //    Als raadgetal hoger: Toon het te raden getal is hoger”  
+                        else if (_raadgetal > _gok)
+                        {
+
+                        }
+                        //	  Als raadgetal lager: Toon het te raden getal is lager”
+                        else
+                        {
+
+                        }
+                    }
+                    //Als 2: Inporteer de tafel van
+                    else if (_keuze == 2)
+                    {
+                    
+                        // begeleiden 
+                        Console.WriteLine("Tot een volgende keer.");
+                        Console.WriteLine("\nDruk op een toets om af te sluiten.");
+                        Console.ReadKey();
+                    }
+
+                    // anders 
+                    else
+                    {
+                        // Scherm leegmaken 
+                        Console.Clear();
+
+                        // foutcode 
+                        Console.WriteLine("U gaf geen juiste keuze in.");
+                        Console.WriteLine("\nDruk op een toets om verder te gaan.");
+                        Console.ReadKey();
+                    }
                 }
                 catch
                 {
@@ -57,6 +105,7 @@ namespace _09_TomA_Raadspel
                     Console.WriteLine("Je gaf geen getal in.");
                     Console.WriteLine("\nDruk op een toets om verder te gaan.");
                     Console.ReadKey();
+                    
                 }
 
             //Stap 6: Ga naar stap 2 tenzij Afsluiten wordt gekozen

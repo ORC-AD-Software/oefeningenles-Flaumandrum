5e30e36e1ffacb8c6a4b746f9e23e5a4ecb36f09
Tom Adriaens <tom.adriaens@romerocollege.be>
2024-11-14 14:30:44 +0100

    [LES] Tot 4 dagen

---
+++ 12_TomA_ToonDate/12_TomA_ToonDate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _12_TomA_ToonDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Tom Adriaens
+            // 14/11/2024
+            // Project: Toon datum
+
+            // Velden 
+            DateTime _nu = DateTime.Now;
+
+            // Programma 
+
+            // Toon de datum.
+            Console.WriteLine(_nu.ToString());
+            Console.WriteLine(_nu.ToString("dddd"));
+
+            _nu = DateTime.Today;
+            Console.WriteLine(_nu);
+
+            _nu = DateTime.Now;
+            Console.WriteLine(_nu.DayOfWeek);
+
+            CultureInfo vlaamseCulture = new CultureInfo("nl-BE");
+            DateTimeFormatInfo dtfi = vlaamseCulture.DateTimeFormat;
+
+            Console.WriteLine($"Vandaag is {dtfi.GetDayName(_nu.DayOfWeek)}");
+
+            Console.WriteLine("Druk op een toets om verder te gaan");
+
+            Console.ReadKey();
+
+        }
+    }
+}

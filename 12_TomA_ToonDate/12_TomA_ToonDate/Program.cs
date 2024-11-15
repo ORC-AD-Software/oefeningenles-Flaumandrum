a248bbdba29a7fabd4389c20e77fbbaf6d6882fc
Tom Adriaens <tom.adriaens@romerocollege.be>
2024-11-15 08:56:54 +0100

    [LES] Tonen van datum afgewerkt

--- 12_TomA_ToonDate/12_TomA_ToonDate/Program.cs
+++ 12_TomA_ToonDate/12_TomA_ToonDate/Program.cs
@@ -23,7 +23,7 @@ namespace _12_TomA_ToonDate
 
             // Toon de datum.
             Console.WriteLine(_nu.ToString());
-            Console.WriteLine(_nu.ToString("dddd"));
+            Console.WriteLine(_nu.ToString("dddd MM yyyy"));
 
             _nu = DateTime.Today;
             Console.WriteLine(_nu);
@@ -35,8 +35,30 @@ namespace _12_TomA_ToonDate
             DateTimeFormatInfo dtfi = vlaamseCulture.DateTimeFormat;
 
             Console.WriteLine($"Vandaag is {dtfi.GetDayName(_nu.DayOfWeek)}");
+            Console.WriteLine("\n");
+
+            Console.WriteLine(_nu.ToShortDateString());
+            Console.WriteLine(_nu.ToString("dd/MM/yyyy"));
+            Console.WriteLine("\n");
+
+            Console.WriteLine(_nu.ToLongDateString());
+            Console.WriteLine(_nu.ToString("dddd dd MMMM yyyy"));
+            Console.WriteLine("\n");
+
+            Console.WriteLine(_nu.ToShortTimeString());
+            Console.WriteLine(_nu.ToString("H:mm"));
+            Console.WriteLine("\n");
+
+            Console.WriteLine(_nu.ToLongTimeString());
+            Console.WriteLine(_nu.ToString("H:MM:ss"));
+
+            Console.WriteLine("\n");
+            Console.WriteLine(_nu.ToString("dd/MM/yyyy HH:mm:ss ffff"));
+            Console.WriteLine("\n");
 
             Console.WriteLine("Druk op een toets om verder te gaan");
+
+            
 
             Console.ReadKey();
 

1243a0f07627fb3f4a894fbb352486774361a29e
Tom Adriaens <tom.adriaens@romerocollege.be>
2025-01-22 11:41:57 +0100

    [LES] Start oef 55

---
+++ 17_TomA_GegArray/17_TomA_GegArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_TomA_GegArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tom Adriaens
+            // 22/01/2025
+            // Project Gegevens in en uit een array
+
+            // Velden 
+
+
+            // Programma
+
+
+        }
+    }
+}

9211c08971f8636218606447b2b1149a6918d9dd
Tom Adriaens <tom.adriaens@romerocollege.be>
2025-01-23 13:12:25 +0100

    [LES] Afgewerkt

---
+++ 18_TomA_ManGegArray/18_TomA_ManGegArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_TomA_ManGegArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tom Adriaens
+            // 22/01/2025
+            // Project mauele gegevens in en uit array
+
+            // Velden 
+            String[] _namen = new String[] { "Jan", "Piet", "Joris", "Corneel" };
+
+            // Programma
+            // Toon de namen 
+            Console.WriteLine(_namen[0]);
+            Console.WriteLine(_namen[1]);
+            Console.WriteLine(_namen[2]);
+            Console.WriteLine(_namen[3]);
+            Console.WriteLine("Die hebben baarden!");
+            
+            // Gebruiker begeleiden
+            Console.WriteLine("\nDruk op enter om af te sluiten");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}

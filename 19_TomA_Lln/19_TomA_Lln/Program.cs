267f9305bd9cd913d2aaf0e510a6e50734177be1
Tom Adriaens <tom.adriaens@romerocollege.be>
2025-01-24 10:00:32 +0100

    [LES] Keuzemenu en trycatch

--- 19_TomA_Lln/19_TomA_Lln/Program.cs
+++ 19_TomA_Lln/19_TomA_Lln/Program.cs
@@ -19,36 +19,67 @@ namespace _19_TomA_Lln
             // Project Leerlingen
 
             // Velden 
+            byte _keuze = 0;
 
             // Programma 
             // Stap 1: Intro
-            Console.WriteLine()
-            //Stap 2: Toon keuzemenu(Klas aanmaken, namen ingeven, tonen, afsluiten)
-            //Stap 3: Vraag keuze +opslaan
-            //Als 1: (klas aanmaken)
-            //		Stap 4: Vraag aantal leerlingen + opslaan
+            Console.WriteLine("Welkom, \nIn dit programma kan je al de namen van je leerlingen ingeven.\nVeel succes.");
+            Console.WriteLine("\nDruk op een enter om te starten");
+            Console.ReadKey();
 
-            //        Stap 5: maar array aan
-            //    Als 2: (namen ingeven)
-            //		Stap 6: Check of er een klas is aangemaakt
-            //Als ok:
-            //            Stap 7: zoek een lege plaats
+            do
+            {
+                //Scherm leegmaken 
+                Console.Clear();
 
-            //    Als ok
-            //Stap 8: Vraag naam +opslaan(op lege plaats)
+                //Stap 2: Toon keuzemenu(Klas aanmaken, namen ingeven, tonen, afsluiten)
+                Console.WriteLine("Maak uw keuze uit onderstaand menu:");
+                Console.WriteLine("\n   1) Klas aanmaken\n   2) Namen ingeven\n   3) Tonen\n   4) Afsluiten");
 
-            //                    Als NOK
+                try
+                {
+                    //Stap 3: Vraag keuze +opslaan
+                    Console.Write("\nUw keuze: ");
+                    _keuze = byte.Parse(Console.ReadLine());
 
-            //                        Stap 9: Foutmelding
-            //            Als NOK
-            //Stap 10: foutmelding
-            //            Als 3: (tonen)
-            //            Stap 11: Toon leerlingen
+                    //Als 1: (klas aanmaken)
+                    //Stap 4: Vraag aantal leerlingen + opslaan
 
-            //    Als 4 : (afsluiten)
-            //        Stap 12: Toon eindtekst
-            //Stap 13: Terug naar stap 2 als keuze niet 4 is
+                    //Stap 5: maar array aan
+                    //Als 2: (namen ingeven)
+                    //Stap 6: Check of er een klas is aangemaakt
+                    //Als ok:
+                    //Stap 7: zoek een lege plaats
 
+                    //    Als ok
+                    //Stap 8: Vraag naam +opslaan(op lege plaats)
+
+                    //                    Als NOK
+
+                    //                        Stap 9: Foutmelding
+                    //            Als NOK
+                    //Stap 10: foutmelding
+                    //            Als 3: (tonen)
+                    //            Stap 11: Toon leerlingen
+
+                    //    Als 4 : (afsluiten)
+                    //        Stap 12: Toon eindtekst
+
+                }
+                catch
+                {
+                    //Scherm leegmaken 
+                    Console.Clear();
+
+                    // Foutmelding 
+                    Console.WriteLine("U gaf geen juist getal in.");
+                    Console.WriteLine("\nDruk op een toets om terug te keren naar het hoofdmenu.");
+                    Console.ReadKey();
+
+                }
+
+                //Stap 13: Terug naar stap 2 als keuze niet 4 is
+            } while (_keuze != 4);
         }
     }
 }

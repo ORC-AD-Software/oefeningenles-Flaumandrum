d88c09124fdf53f9c7f7dfb9c0014c77d08e3784
Tom Adriaens <tom.adriaens@romerocollege.be>
2025-02-12 10:50:45 +0100

    [LES] Toevoegen functies

---
+++ 23_TomA_Boodschappenlijst2Lagen/23_TomA_Boodschappenlijst2Lagen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_TomA_Boodschappenlijst2Lagen
+{
+    internal class Program
+    {
+        // Tom Adriaens
+        // 12/02/2025
+        // Project Boodschappenlijst 2 lagen
+
+        // Velden 
+
+
+        // GUI
+        static void Main(string[] args)
+        {
+            
+
+             
+        }
+
+        // Functies 
+        int ZoekInArray(String ontvZoekItem)
+        {
+            int antwoord = 0;
+
+            return antwoord;
+        }
+        void Toevoegen(String ontvItem, int ontvPlaats)
+        {
+
+        }
+
+        String ToonLijst()
+        {
+            string antwoord = "";
+
+            return antwoord;
+        }
+
+    }
+}

f92bfd827fc9ce4495932b0fe71698ddda21e369
Tom Adriaens <tom.adriaens@romerocollege.be>
2025-04-23 11:00:02 +0200

    Start oefening + commentaren + velden aangemaakt

---
+++ 24_TomA_BroodjesBest_2L/24_TomA_BroodjesBest_2L/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_TomA_BroodjesBest_2L
+{
+    class Program
+    {
+        // Tom Adriaens 
+        // 23/04/2025
+        // Project: Bestelling broodjes
+
+        // Velden 
+        static String[] _naam = new String[5];
+        static String[] _broodje = new String[5];
+        static Double[] _prijs = new Double[5];
+        static int _indexWinnaar = -1;
+
+
+
+        // GUI
+        static void Main(string[] args)
+        {
+            
+
+            
+
+            
+
+            
+
+
+        }
+
+        //Business
+
+    }
+}
